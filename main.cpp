1ae7ce410c18a5c06254bc5565249e836ea5fe23
sribalan98 <105964659+sribalan98@users.noreply.github.com>
2025-12-28 13:53:59 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,6 @@
 
 using namespace std;
 
-
-
-
-
 struct User {
     string name;
     int id;
@@ -27,12 +23,15 @@ public:
         this->data.push_back(user); // push back method from vector.h
     };
 
-    void getAllUsers() {
-        for (const auto& user : this->data) {
-            cout << "{" << " Name : " << user.name << "; "
-            << "Id : " << user.id  << "; " << "Balance : "
-            << user.balance << "; " << "}" << endl;
+    void setUserBalance(int userId, int amount) {
+        for (auto& user : this->data) {
+            if (user.id == userId) {
+                user.balance += amount;
+            }
         }
+    }
+    vector<User> getAllUsers() {
+        return data;
     };
 
     int getBankWealth() const {
@@ -43,11 +42,25 @@ public:
         return amount;
     }
 
+    User getUserById(int id) const {
+        for (const auto& user : data) {
+            if (user.id == id) {
+
+                return user;
+            }
+        }
+    }
 };
+
 int backspace();
 int menufn();
 void getBankAmount(const Bank & bank);
 void addUser(Bank & bank);
+void getAllUsers(Bank& bank);
+void getUserById(Bank & bank);
+void creditmoneyTouser(Bank & bank);
+void debitmoneyTouser(Bank& bank);
+
 
 int main()
 {
@@ -65,9 +78,24 @@ int main()
                 backspace();
                 break;
             case 3:
-                bank.getAllUsers();
+                getAllUsers(bank);
+                // bank.getAllUsers();
                 backspace();
                 break;
+            case 4:
+                getUserById(bank);
+                backspace();
+                break;
+            case 5:
+                creditmoneyTouser(bank);
+                backspace();
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 0:
+                exit(0);
             default:
                 exit(0);
 
@@ -77,12 +105,14 @@ int main()
 }
 
 int backspace() {
+    again:
     int back;
     cout << "Press 0 to back to Menu : " << endl;
     cin >> back;
     if (back == 0) {
         return back;
     }
+    goto again;
 }
 int menufn() {
     int selection;
@@ -112,4 +142,56 @@ void addUser(Bank& bank) {
     cout << "Enter the User Name  : " << endl;
     cin >> name;
     bank.setUser(name , openingBalance);
+}
+
+void getAllUsers(Bank& bank) {
+    vector<User> users;
+    users  = bank.getAllUsers();
+    if (!users.empty()) {
+        for (const auto& user : users) {
+            cout << "{" << " Name : " << user.name << "; "
+            << "Id : " << user.id  << "; " << "Balance : "
+            << user.balance << "; " << "}" << endl;
+        }
+    } else {
+    cout << "There is no users in the Bank" << endl;
+    }
+}
+void getUserById(Bank& bank) {
+    int id;
+    cout << "Enter User ID : " << endl;
+    cin >> id;
+    User user = bank.getUserById(id);
+    cout << "{" << " Name : " << user.name << "; "
+                << "Id : " << user.id  << "; " << "Balance : "
+                << user.balance << "; " << "}" << endl;
+}
+
+
+void creditmoneyTouser(Bank& bank) {
+    int userId;
+    int creditmoney;
+    cout << "Enter User ID : " << endl;
+    cin >> userId;
+    User user = bank.getUserById(userId);
+    cout <<  "The UserName  is : " << user.name << "; " << endl;
+    cout << "Enter the Amount to Credit to the user : ";
+    cin >> creditmoney;
+    cout << endl;
+    bank.setUserBalance(userId, creditmoney);
+}
+
+
+
+void debitmoneyTouser(Bank& bank) {
+    int userId;
+    int debitmoney;
+    cout << "Enter User ID : " << endl;
+    cin >> userId;
+    User user = bank.getUserById(userId);
+    cout <<  "The UserName  is : " << user.name << "; " << endl;
+    cout  << "Enter the Amount to Debit to the user : ";
+    cin >> debitmoney;
+    cout << endl;
+    bank.debitAmountToUser(userId, debitmoney);
 }
